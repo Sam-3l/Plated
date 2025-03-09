5b6a587e412dfbe9410281bb9a3416d71bdaedf9
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-03-09 18:27:52 +0100

    buff

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -166,5 +166,3 @@ class PublicUserViewSet(viewsets.ModelViewSet):
         else:
             user.followers.add(auth_user)
             return Response({'detail': f'{user.username} followed'}, status=status.HTTP_201_CREATED)
-        
-    # next tomorrow
